5949b60019e96d24c360286252e4f09a2bf20031
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-01-24 12:54:35 +0800

    Renamed file, greet, exit

---
+++ src/main/java/Huhuharis.java
@@ -0,0 +1,7 @@
+public class Huhuharis {
+    public static void main(String[] args) {
+        System.out.println("Hello! I'm Huhuharis");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
