5ba79574e259d02eecea0b5dad8d8db7868ea4ee
mhaikelll <126130091+mhaikelll@users.noreply.github.com>
2025-01-24 13:13:01 +0800

    Renamed file

--- src/main/java/Huhuhuharis.java
+++ src/main/java/Huhuhuharis.java
@@ -1,6 +1,6 @@
-public class Huhuharis {
+public class Huhuhuharis {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Huhuharis");
+        System.out.println("Hello! I'm Huhuhuharis");
         System.out.println("What can I do for you?");
         System.out.println("Bye. Hope to see you again soon!");
     }
